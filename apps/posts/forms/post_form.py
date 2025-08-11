4757c89586f89b5c87c6b824330d92ecad7a36af
Alan Zanazzo <alanzanazzo44@gmail.com>
2025-08-11 00:58:00 -0300

    Vista previa de portado al crear post

--- apps/posts/forms/post_form.py
+++ apps/posts/forms/post_form.py
@@ -4,7 +4,7 @@ from apps.posts.models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['title', 'abstract', 'cover', 'body']  # ajustá según tus campos
+        fields = ['title', 'abstract', 'category', 'cover', 'body']
         widgets = {
             'title': forms.TextInput(attrs={
                 'class': 'w-full px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500',
@@ -13,6 +13,9 @@ class PostForm(forms.ModelForm):
             'abstract': forms.Textarea(attrs={
                 'class': 'w-full px-4 py-2 border rounded-md h-24 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500',
                 'placeholder': 'Resumen breve...'
+            }),
+            'category': forms.Select(attrs={
+                'class': 'w-full px-4 py-2 border rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-blue-500',
             }),
             'cover': forms.ClearableFileInput(attrs={
                 'class': 'w-full px-4 py-2 border rounded-md bg-white'
